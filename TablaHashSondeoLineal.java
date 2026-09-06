2b90113476eedbded5474db535b4cab1cbb383f5
Poncho677 <alfonso.gb@ciencias.unam.mx>
2026-09-05 21:31:18 -0600

    Add files via upload
    Punto Extra

---
+++ TablaHashSondeoLineal.java
@@ -0,0 +1,154 @@
+import java.util.ArrayList;
+
+public class TablaHashSondeoLineal{
+
+    private ArrayList<Nodo>[] tabla;
+    private int m = 7;
+    private int n = 0;
+    private static final String DELETED = "DELETED";
+    
+    private class Nodo{
+	private int llave;
+	private String valor;
+
+	private Nodo(int llave, String valor){
+	    this.llave = llave;
+	    this.valor = valor;
+	}
+
+	public int getLlave(){
+	    return llave;
+	}
+
+	public String getValor(){
+	    return valor;
+	}
+
+	public boolean isDeleted(){
+	    return valor.equals(DELETED);
+	}
+
+	@Override
+	public String toString(){
+	    return "(" + llave + ", " + valor.toString() + ")";
+	}
+    }
+
+    @SuppressWarnings("unchecked")
+    public TablaHashSondeoLineal(){
+        tabla = new ArrayList[m];
+        for (int i = 0; i < m; i++) {
+            tabla[i] = null;
+        }
+    }
+
+    private int hash(int l){
+	return Math.abs(l) % m;
+    }
+
+    public void insertar(int llave, String valor) {
+	int indice = hash(llave);
+	int primerBorrado = -1;
+	for (int i = 0; i < m; i++) {
+            int posicion = (indice + i) % m;
+	    if (tabla[posicion] != null) {
+                Nodo nodo = tabla[posicion].get(0);
+		if (nodo.isDeleted()) {
+                    if (primerBorrado == -1) {
+                        primerBorrado = posicion;
+                    }
+		} else if (nodo.getLlave() == llave) {
+		    nodo.valor = valor;
+		    return;
+		}
+	    } else {
+		int destino = posicion;
+		if (primerBorrado != -1) {
+		    destino = primerBorrado;
+		}
+		tabla[destino] = new ArrayList<>();
+		tabla[destino].add(new Nodo(llave, valor));
+		n++;
+		return;
+	    }
+	}
+	if (primerBorrado != -1) {
+            tabla[primerBorrado] = new ArrayList<>();
+            tabla[primerBorrado].add(new Nodo(llave, valor));
+            n++;
+            return;
+        }
+        System.out.println("ERROR: Tabla llena");
+    }
+	    
+    public void buscar(int llave){
+	int busca = hash(llave);
+	for (int i = 0; i < m; i++) {
+            int posicion = (busca + i) % m;
+	    if (tabla[posicion] == null) {
+                System.out.println("NOT_FOUND");
+                return;
+            }
+	    Nodo nodo = tabla[posicion].get(0);
+	    if (nodo.isDeleted()) {
+                continue;
+            }
+	    if (nodo.getLlave() == llave) {
+                System.out.println(posicion + " -> " + nodo.getValor());
+                return;
+            }
+        }
+	System.out.println("NOT_FOUND");
+    }
+
+    public int contiene(int llave){
+	int busca = hash(llave);
+	for (int i = 0; i < m; i++) {
+            int posicion = (busca + i) % m;
+	    if (tabla[posicion] == null) {
+                return -1;
+            }            
+            Nodo nodo = tabla[posicion].get(0);
+	    if (nodo.isDeleted()) {
+                continue;
+            }
+	    if (nodo.getLlave() == llave) {
+                return posicion;
+            }
+        }
+	return -1;
+    }
+
+
+    public void eliminar(int llave){
+        int posicion = contiene(llave);
+        if (posicion != -1) {
+            tabla[posicion] = new ArrayList<>();
+            tabla[posicion].add(new Nodo(-1, DELETED));
+            n--;
+        }
+    }
+
+    public double factorCarga() {
+	return (double) n / m;
+    }
+
+    @Override
+    public String toString(){
+	String s = "";
+	for(int i = 0; i < m; i++){
+	    s += i + " -> ";
+	    if(tabla[i] == null) {
+		s += "\n";
+	    } else {
+	        Nodo nodo = tabla[i].get(0);
+                if (nodo.isDeleted()) {
+                    s += "DELETED\n";
+                } else {
+                    s += nodo.toString() + "\n";
+                }
+            }
+        }
+        return s;
+    }
+}
